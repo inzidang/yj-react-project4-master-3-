d67d747dfa9dfb214effa68868345c793744bd93
이연경 <inzidang@gmail.com>
2023-08-07 13:12:44 +0900

    first commit

---
+++ src/api.js
@@ -0,0 +1,33 @@
+const BASE_PATH = process.env.NODE_ENV === "production" ? "https://port-0-yj-react-project5-back-ac2nll07a5ey.sel4.cloudtype.app":"http://localhost:8080";
+
+export async function rentalNotices() {
+  return await fetch(`${BASE_PATH}/api/rental/notice`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  }).then((res) => res.json());
+}
+
+export async function rentalNoticeDetail(props) {
+  const id = props.queryKey[1];
+  return await fetch(`${BASE_PATH}/api/rental/notice-detail/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  }).then((res) => res.json());
+}
+
+export async function rentalNoticeWrite(props) {
+  return await fetch(`${BASE_PATH}/api/rental/notice-write`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(props),
+  }).then((res) => res.json());
+}
